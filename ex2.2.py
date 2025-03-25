746fb57d47d7a1505a12f27c27f8d69fedb3ea58
Jingyan-0112 <s11350027@cdjh.tp.edu.tw>
2025-03-25 20:31:26 +0800

    Create ex2.2.py

---
+++ ex2.2.py
@@ -0,0 +1,8 @@
+.........................
+this is hello python progran
+.........................
+'''
+printf("Hello") #this is my frist progran 
+#this is my frist progran
+print(2+3)
+print(9+9)
